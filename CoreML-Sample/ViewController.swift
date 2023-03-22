09ed39c2c0866b4e1c1f5a36402652c822d0b288
OONISHI REON <oonishireon110@gmail.com>
2023-03-22 21:17:11 +0900

    first commit

---
+++ CoreML-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreML-Sample
+//
+//  Created by 大西 玲音 on 2023/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
